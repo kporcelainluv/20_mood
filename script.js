a66b4deafc4d3ce05e88a5b238f07199c209d5dd
Ksusha Zhukova <zhukovaksusha@gmail.com>
2018-02-25 23:27:04 +0300

    update logic, update var names, create css

--- script.js
+++ script.js
@@ -1,22 +1,38 @@
-var quotes = JSON.parse(document.getElementById('quotes_json').innerHTML);
+function getButtonHandler() {
+  var quotes = [{
+      "phrase": "Кукурузные хлопья - теперь без асбеста!",
+      "signature": "©Неизвестный маркетолог"
+    },
+    {
+      "phrase": "Собираюсь жить вечно, пока всё идёт нормально"
+    },
+    {
+      "phrase": "Вывеска над кафе — '\u003cМы открылись\u003e'. Что-то не так. Где же '\u003c/Мы открылись\u003e'?"
+    },
+    {
+      "phrase": "Если ты будешь усердно работать по 8 часов в день, то обязательно станешь боссом и начнешь работать по 12"
+    }
+  ]
 
-function get_text(i) {
-    if (quotes[i]["signature"] === undefined) {
-        text = quotes[i]["phrase"];
-  } else {
-        text = quotes[i]["phrase"] + " " + quotes[i]["signature"];
+  function get_text(index) {
+    const quote = quotes[index];
+    if (quote.signature) {
+      return `${quote.phrase} ${quote.signature}`;
+    }
+    return quote.phrase
   }
-  return text
-}
 
-var new_li_element = document.createElement('li');
-new_li_element.innerHTML = get_text(0)
-quote_block.appendChild(new_li_element);
+  const quote_node = document.getElementById('quote');
+  quote_node.innerHTML = get_text(0)
 
-var num_of_quote = 1;
+  var num_of_quote = 1;
 
-function button_click() {
-    new_li_element.innerHTML = get_text(num_of_quote)
+  return function button_click() {
+    quote_node.innerHTML = get_text(num_of_quote)
     num_of_quote += 1
     num_of_quote = num_of_quote % quotes.length
+
+  }
 }
+
+document.querySelector(".button").addEventListener("click", getButtonHandler())
